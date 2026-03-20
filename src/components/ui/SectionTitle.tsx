5af5baa2e46daa7297fff45ef52d18f868d35ea9
JohnSalinas123 <salinasjohn257@gmail.com>
2026-03-19 19:15:34 -0700

    add heading type and fontSize props optional

--- src/components/ui/SectionTitle.tsx
+++ src/components/ui/SectionTitle.tsx
@@ -1,14 +1,19 @@
+import type { CSSProperties } from "react";
 
 interface SectionTitleProps {
     text: string;
+    as?: "h1" | "h2" | "h3" | "h4";
+    fontSize?: CSSProperties['fontSize'];
 }
 
 export const SectionTitle = ({
-    text
+    text,
+    as: Tag = "h2",
+    fontSize
 }: SectionTitleProps) => {
     return (
         <>
-            <h2 className="section-title">{text}</h2>   
+            <Tag className="section-title" style={{fontSize : fontSize}}>{text}</Tag>   
         </>
     )
 }   
